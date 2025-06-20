75584ec1f943ca507ed87321a347fb3c8150290e
bhatt-ayush999 <bhattayush598@gmail.com>
2025-06-20 17:13:01 +0000

    adding variables

---
+++ login/form.js
@@ -0,0 +1,9 @@
+//user deatils in gpt login form
+let userMail = "abc@xmail.com"
+let userPassword = 1234
+let loginOptions ="google"
+
+//checking there datatypes
+console.log(typeof userMail)
+console.log(typeof userPassword)
+console.log(typeof loginOptions)
